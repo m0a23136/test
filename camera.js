e2adc79de0bc50039db5c1fd32e42e8dde07fd47
kanamaui <kanaha.kh6@gmail.com>
2026-06-09 13:23:08 +0900

    カメラ切り替え追加

--- camera.js
+++ camera.js
@@ -4,21 +4,52 @@ const captureBtn = document.getElementById('captureBtn');
 const gallery = document.getElementById('gallery');
 const tagInput = document.getElementById('tagInput');
 const searchInput = document.getElementById('searchInput');
+const switchCameraBtn =
+  document.getElementById('switchCameraBtn');
+
+let currentCamera = "environment";
+let currentStream = null;
 
 // カメラ起動
 async function startCamera() {
+
   try {
+
+    // 既存カメラ停止
+    if (currentStream) {
+      currentStream.getTracks().forEach(track => {
+        track.stop();
+      });
+    }
+
     const stream = await navigator.mediaDevices.getUserMedia({
-      video: true,
+      video: {
+        facingMode: currentCamera
+      },
       audio: false
     });
 
+    currentStream = stream;
     video.srcObject = stream;
+
   } catch (error) {
-    alert('カメラにアクセスできません');
+    alert("カメラにアクセスできません");
     console.error(error);
   }
 }
+
+switchCameraBtn.addEventListener('click', () => {
+
+  if (currentCamera === "environment") {
+    currentCamera = "user";
+  } else {
+    currentCamera = "environment";
+  }
+
+  startCamera();
+
+});
+
 // LocalStorageから取得
 function getPhotos() {
   return JSON.parse(localStorage.getItem('photos')) || [];
