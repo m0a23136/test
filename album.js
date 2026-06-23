f54d81dc7c08513a43424da4f0ed6ed817237d26
kanamaui <kanaha.kh6@gmail.com>
2026-06-23 13:25:05 +0900

    6/23　変更

---
+++ album.js
@@ -0,0 +1,81 @@
+renderPhotos();
+
+searchInput.addEventListener('input', (e) => {
+    renderPhotos(e.target.value);
+});
+
+// 写真表示
+function renderPhotos(filterTag = '') {
+  const photos = getPhotos();
+  gallery.innerHTML = '';
+
+  const filtered = photos.filter(photo => {
+    if (!filterTag) return true;
+
+    return photo.tags.some(tag =>
+      tag.toLowerCase().includes(filterTag.toLowerCase())
+    );
+  });
+
+  filtered.reverse().forEach(photo => {
+    const card = document.createElement('div');
+    card.className = 'photo-card';
+
+   card.innerHTML = `
+     <img src="${photo.image}" alt="photo">
+     <div class="photo-info">
+      <div class="tags">
+       ${photo.tags.map(tag => `#${tag}`).join(' ')}
+      </div>
+
+     <div class="date">
+      ${photo.date}
+     </div>
+
+     <button class="delete-btn" data-id="${photo.id}">
+      削除
+     </button>
+     </div>
+    `;
+
+    gallery.appendChild(card);
+    updateMemoryGauge();
+  });
+
+  document.querySelectorAll('.delete-btn').forEach(button => {
+
+   button.addEventListener('click', () => {
+     const id = Number(button.dataset.id);
+     const photos = getPhotos();
+     const updatedPhotos = photos.filter(
+      photo => photo.id !== id  
+     );
+
+     savePhotos(updatedPhotos);
+
+     renderPhotos(searchInput.value);
+     //console.log("押された");
+     console.log(button.dataset.id);
+   });
+
+  });
+}
+
+
+function updateMemoryGauge() {
+
+    const photos = getPhotos();
+
+    const maxPhotos = 30;   //ゲージの最大値
+
+    const percent = Math.min(
+        photos.length / maxPhotos * 100,
+        100
+    );
+
+    document.getElementById("memoryBar").style.width =
+        percent + "%";
+
+    document.getElementById("memoryText").textContent =
+        `撮った写真：${photos.length}枚`;
+}
--- camera.js
+++ camera.js
@@ -60,61 +60,6 @@ function savePhotos(photos) {
   localStorage.setItem('photos', JSON.stringify(photos));
 }
 
-// 写真表示
-function renderPhotos(filterTag = '') {
-  const photos = getPhotos();
-  gallery.innerHTML = '';
-
-  const filtered = photos.filter(photo => {
-    if (!filterTag) return true;
-
-    return photo.tags.some(tag =>
-      tag.toLowerCase().includes(filterTag.toLowerCase())
-    );
-  });
-
-  filtered.reverse().forEach(photo => {
-    const card = document.createElement('div');
-    card.className = 'photo-card';
-
-   card.innerHTML = `
-     <img src="${photo.image}" alt="photo">
-     <div class="photo-info">
-      <div class="tags">
-       ${photo.tags.map(tag => `#${tag}`).join(' ')}
-      </div>
-
-     <div class="date">
-      ${photo.date}
-     </div>
-
-     <button class="delete-btn" data-id="${photo.id}">
-      削除
-     </button>
-     </div>
-    `;
-
-    gallery.appendChild(card);
-  });
-
-  document.querySelectorAll('.delete-btn').forEach(button => {
-
-   button.addEventListener('click', () => {
-     const id = Number(button.dataset.id);
-     const photos = getPhotos();
-     const updatedPhotos = photos.filter(
-      photo => photo.id !== id  
-     );
-
-     savePhotos(updatedPhotos);
-
-     renderPhotos(searchInput.value);
-     console.log("押された");
-     console.log(button.dataset.id);
-   });
-
-  });
-}
 
 // 撮影
 captureBtn.addEventListener('click', () => {
@@ -145,15 +90,13 @@ captureBtn.addEventListener('click', () => {
 
   tagInput.value = '';
 
-  renderPhotos(searchInput.value);
+  alert("保存しました")
 });
 
-// タグ検索
-searchInput.addEventListener('input', (e) => {
-  renderPhotos(e.target.value);
-});
+
 
 // 初期化
 startCamera();
-renderPhotos();
+
+
 
