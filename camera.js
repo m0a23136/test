9e7caac850fac4adf7476aa3681afc5b2cbd068b
kanamaui <kanaha.kh6@gmail.com>
2026-09-24 13:10:08 +0900

    縦横比直した

--- camera.js
+++ camera.js
@@ -60,23 +60,23 @@ switchCameraBtn.addEventListener('click', () => {
 captureBtn.addEventListener('click', () => {
   const context = canvas.getContext('2d');
 
- // 保存する画像サイズ
- const width = 640;
- const height = 480;
+  // カメラ映像本来のサイズを使用
+  const width = video.videoWidth;
+  const height = video.videoHeight;
 
- canvas.width = width;
- canvas.height = height;
+  canvas.width = width;
+  canvas.height = height;
 
- // カメラ映像を縮小して描画
- context.drawImage(video, 0, 0, width, height);
+  // カメラ映像をそのまま保存
+  context.drawImage(video, 0, 0, width, height);
 
- shutterSound.currentTime = 0;
- shutterSound.play(); 
+  shutterSound.currentTime = 0;
+  shutterSound.play();
 
- // JPEG形式
- const imageData = canvas.toDataURL('image/jpeg');
+  // JPEG形式
+  const imageData = canvas.toDataURL('image/jpeg');
 
-   // 撮影した写真を一時保存
+  // 撮影した写真を一時保存
   pendingPhoto = imageData;
 
   // メモ入力
@@ -88,18 +88,19 @@ captureBtn.addEventListener('click', () => {
     return;
   }
 
-  //保存
+  // タグ
   const tags = tagInput.value
     .split(' ')
     .map(tag => tag.trim())
     .filter(tag => tag !== '');
 
+  // 保存するデータ
   const newPhoto = {
-   id: Date.now(),
-   image: imageData,
-   tags: tags,
-   memo:memo,
-   date: new Date().toLocaleString('ja-JP')
+    id: Date.now(),
+    image: imageData,
+    tags: tags,
+    memo: memo,
+    date: new Date().toLocaleString('ja-JP')
   };
 
   const photos = getPhotos();
@@ -109,8 +110,7 @@ captureBtn.addEventListener('click', () => {
   tagInput.value = '';
   pendingPhoto = null;
 
-
-  alert("保存しました")
+  alert("保存しました");
 });
 
 //アルバムをひらく
